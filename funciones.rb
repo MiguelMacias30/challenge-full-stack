3fadee7b26bf7770fbd5d8b0b2b7d305cee38c17
miguelfernandomacias@gmail.com <miguelfernandomacias@gmail.com>
2021-05-28 23:50:37 -0400

    firts exercise

---
+++ funciones.rb
@@ -0,0 +1,12 @@
+# 1 Ejercicio
+def intersection_union(m1,m2)
+    if  m1.length > 1 && m2.length > 1
+        mi= m1&m2
+        mu= m1|m2 
+        print "intersection_union(#{m1},#{m2}) -> [#{mi.sort},#{mu.sort}]"
+    else
+        puts ('las matrices deben tener al menos un elemento')
+    end
+end
+intersection_union([2,5],[4,8,3,1,7])
+puts
