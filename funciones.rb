ec003290c1dbe1d17c3511475eb487e2da104a87
miguelfernandomacias@gmail.com <miguelfernandomacias@gmail.com>
2021-05-28 23:51:37 -0400

    second exercise

--- funciones.rb
+++ funciones.rb
@@ -10,3 +10,15 @@ def intersection_union(m1,m2)
 end
 intersection_union([2,5],[4,8,3,1,7])
 puts
+
+#2 Ejercicio
+def find(m)
+    ubicacion = m.index("Bob")
+    if ubicacion == nil 
+        print "-1"
+    else
+        print "find_bob(#{m}) -> #{ubicacion}"
+    end
+end
+find(["lourdes","lauren","Bob","lindsey"])
+puts
